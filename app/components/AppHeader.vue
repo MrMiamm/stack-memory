107326b174c8c622ca374e94a75e63fff7a289e1
MrMiamm <cherwill@laposte.net>
2025-11-21 00:31:57 +0100

    Header

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="flex flex-row justify-between items-center gap-2">
+    <GoBackButton :class="[$route.path === '/' && 'opacity-0 pointer-events-none']"/>
+    <h1>stack-memory</h1>
+    <color-switcher/>
+  </header>
+</template>
+
+<script setup lang="ts">
+import GoBackButton from './buttons/GoBackButton.vue';
+
+</script>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,15 +1,10 @@
 <template>
   <div id="app" class="min-h-screen px-4 py-2 sm:px-6 sm:py-4">
-    <header class="flex flex-row justify-between items-center gap-2">
-      <GoBackButton v-if="($route.path !== '/')"/><div v-else />
-      <color-switcher/>
-    </header>
+    <app-header/>
     <slot />
-    
   </div>
 </template>
 
 <script lang="ts" setup>
-import GoBackButton from '~/components/buttons/GoBackButton.vue';
 
 </script>
